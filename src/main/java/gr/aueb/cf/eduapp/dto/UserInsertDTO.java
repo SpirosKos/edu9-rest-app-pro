c25f038312d322850befa212e5ad6b9f1b3bdd40
sprs <spiroskos9@gmail.com>
2026-03-03 18:53:20 +0200

    fix: change @NotBlank to @NotNull for roleid

--- src/main/java/gr/aueb/cf/eduapp/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/eduapp/dto/UserInsertDTO.java
@@ -1,6 +1,6 @@
 package gr.aueb.cf.eduapp.dto;
 
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -15,7 +15,7 @@ public record UserInsertDTO(
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=])^.{8,}$")
         String password,
 
-        @NotBlank
+        @NotNull
         Long roleId
 ) {
 }
